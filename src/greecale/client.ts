a4483582bd355f5ff03e31ff1d145866f05a7816
Fabricio Parola <fabricio.parola@gmail.com>
2025-07-03 14:10:55 -0300

    --ammend

--- src/greecale/client.ts
+++ src/greecale/client.ts
@@ -38,34 +38,3 @@ export class SDKClient {
 
   // --- SHARED --- //
 }
-
-(async () => {
-  const client = new SDKClient({
-    baseURL: "https://apis-uat.fastpays.com.br/api",
-    credentials: keys["produto-50"],
-  });
-
-  const token = await client.authenticate();
-  const options = { token };
-  logger({ token });
-
-  // --- crypto ---
-  // const aesKey = await client.crypto.getAESKey(options);
-  // logger({ aesKey });
-
-  // const toEncrupt = "5540542841359729";
-  // const encrypted = await client.crypto.encrypt(toEncrupt, options);
-  // logger({ encrypted });
-
-  // const encrypted = "5CD2F824502B11CEED5B235ADE2EB8FF";
-  // const decrypted = await client.crypto.decrypt(encrypted, options);
-  // logger({ decrypted });
-
-  // --- portador (card-holder) ---
-  newPortadorDTO.cpf = "24406570004";
-  const novo_portador = await client.portador.add(newPortadorDTO, options);
-  logger({ novo_portador });
-
-  // const portador = await client.portador.getById("1014500000226001", options);
-  // logger({ portador });
-})();
